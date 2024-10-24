3a6d0986057a0bd3b69fe140e59a26df609b543c
NicolasF425 <nifan42@gmail.com>
2024-10-24 11:39:10 +0200

    phase 1 v1.0

--- p2_phase1.py
+++ p2_phase1.py
@@ -1,24 +1,45 @@
+# IMPORTS
 from bs4 import BeautifulSoup as bs
 import requests as rq
 
+titre_livre = ""
+prix_livre = 0
 
 # URL de la page qui nous intéresse
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
+
 # récupère les données html de la page
-result = rq.get(url)
+page = rq.get(url)
 
+if page.status_code == 200:   # code 200 = OK
+    
+    # pour recherche dans le code html
+    soup = bs(page.content, 'html.parser')
 
-if result.status_code == 200:   # code 200 = OK
-    
-    # affichage du code source de la page
-    print (result.text)
-    
+    # recheche sur la balise <li> et la classe "active"
+    # car combinaison unique sur la page
+    # puis on ne garde que le contenu utile
+    titre_livre = soup.find("li", class_="active").get_text()
+
+    # recheche sur la balise <p> et la classe "price_color"
+    # car combinaison unique sur la page
+    # puis on ne garde que le contenu utile
+    prix_livre = soup.find("p", class_="price_color").get_text()
+    # suppression de la monnaie utilisée
+    prix_livre = prix_livre[1:len(prix_livre)-1]
+
+    print("Titre du livre : ", titre_livre)
+    print("Prix : ", prix_livre)
+ 
     # ouvre un fichier source.txt en écriture ou le créé
-    f = open('source.txt', 'w')
+    #f = open('livre.txt', 'w')
     # écrit les données de la page dans le fichier source.txt
-    f.write(result.text)
+    #f.write(page.content)
 
 else:
     print ("Erreur")
+
+
+
 
