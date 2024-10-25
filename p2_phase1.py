0aa8f5e128ca4cc9ecb9dfc299ac2efa8f4c508f
NicolasF425 <nifan42@gmail.com>
2024-10-25 14:17:04 +0200

    phase 1 v3.0

--- p2_phase1.py
+++ p2_phase1.py
@@ -20,6 +20,7 @@ url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html
 # review_rating
 # image_url
 # PARAMETRE : l'url du produit
+# RETOUR : une liste des elementsdemandes
 def get_book_infos(url):
     liste_infos_livre = [""]
     # récupère les données html de la page
@@ -30,30 +31,71 @@ def get_book_infos(url):
         # pour recherche dans le code html
         soup = bs(page.content, 'html.parser')
 
-        # recheche sur la balise <li> et la classe "active"
-        # car combinaison unique sur la page
-        # puis on ne garde que le contenu utile
-        titre_livre = soup.find("li", class_="active").get_text()
+        # recheche sur la balise <li> et la classe "active" car combinaison unique sur la page
+        # puis on ne garde que le contenu utile (texte)
+        title = soup.find("li", class_="active").get_text()
 
-        # recheche sur la balise <p> et la classe "price_color"
-        # car combinaison unique sur la page
-        # puis on ne garde que le contenu utile
-        prix_livre = soup.find("p", class_="price_color").get_text()
-        # suppression de la monnaie utilisée
-        prix_livre = prix_livre[1:len(prix_livre)-1]
+        # recherche et recuperation de l'UPC, du prix HT et du prix TTC 
+        # selection de la balise <table> et de la classe "table table-striped" car combinaison unique sur la page
+        infos_brut = soup.find("table", class_="table table-striped")
+        # print(infos_brut)
+        # recherche des balise <td> contenant les infos
+        infos_liste = infos_brut.findAll("td")
+        # suppression des balises
+        liste_textes = []
+        for info in infos_liste:
+            liste_textes.append(info.get_text())
+        # print(infos_liste)
+        # print(liste_textes)
 
+       
+        # affectation des infos
+        UPC = liste_textes[0]
+        price_exc_tax = liste_textes[2]
+        price_exc_tax = price_exc_tax[1:len(price_exc_tax)-1]   # suppression du texte de la monnaie
+        price_inc_tax = liste_textes[3]
+        price_inc_tax = price_inc_tax[1:len(price_inc_tax)-1]   # suppression du texte de la monnaie
+        number_available = liste_textes[5]
+
+
+        # récuperation du descriptif du livre
+        balises_p = soup.find_all("p")
+        balises = []
+        for balise in balises_p:
+            balises.append(balise.get_text())   # suppression des balises dans la chaine de caracteres
+        # print(balises_p)
+        book_desc = balises[len(balises)-1] # le descriptif est le dernier de la liste
+        # print(book_desc)
+        
+        # recherche et recuperarion de la categorie
+        # selection de la balise <ul> et de la classe "breadcrumb" car combinaison unique sur la page
+        search = soup.find("ul", class_="breadcrumb")
+        # recheche des balises <a> dans cette recherche
+        liste_a = search.find_all("a")
+        liste_str = []
+        for i in liste_a:
+            liste_str.append(i.get_text())
+        category = liste_str[2]
+        print(category)
+        
         # creation de la liste avec les infos du livres
         # on commence par l'url et on ajoute les autres infos
         liste_infos_livre[0] = url
-        liste_infos_livre.append(titre_livre)
-        liste_infos_livre.append(prix_livre)
+        liste_infos_livre.append(UPC)
+        liste_infos_livre.append(title)
+        liste_infos_livre.append(price_exc_tax)
+        liste_infos_livre.append(price_inc_tax)
+        liste_infos_livre.append(number_available)
+        liste_infos_livre.append(book_desc)
+        liste_infos_livre.append(category)
 
+        
         return liste_infos_livre
 
     else:
         print("Erreur")
 
-# test d'ecriture avec un livre
+# ecrit les infos sur un livre dans un fichier
 def write_book_csv(liste_infos):
     header = "product_page_url,UPC,title,price_including_tax,price_excluding_tax,number_available,product_description," \
             "category,review_rating,image_url\n"
@@ -69,8 +111,13 @@ def write_book_csv(liste_infos):
     for i in liste_infos:
         ligne += i
         ligne += ","
+    # suppression de la virgule de fin et remplacement par \n
+    #
+
+    # ecriture dans le fichier des infos sur le livre
     f.write(ligne)
 
 
+# EXECUTION DU PROGRAMME
 liste_infos = get_book_infos(url)
 write_book_csv(liste_infos)
