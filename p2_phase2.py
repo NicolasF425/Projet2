32c02ca187359a3da03fe3b04540d256eac399f4
NicolasF425 <nifan42@gmail.com>
2024-11-01 11:08:30 +0100

    test

--- p2_phase2.py
+++ p2_phase2.py
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
 # IMPORTS
 from bs4 import BeautifulSoup as bs
 import requests as rq
-from p2_phase1 import get_book_infos, write_header_csv, write_book_csv
+from p2_phase1 import get_book_infos, write_book_csv
+import os
+from datetime import datetime as dt
 
 # 
 header = "product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description," \
@@ -67,74 +68,31 @@ def books_infos_by_category(url):
             print("Page categorie " + url + "non trouvée")
     print(str(total_articles) + " elements")
 
-=======
-# IMPORTS
-from bs4 import BeautifulSoup as bs
-import requests as rq
-from p2_phase1 import get_book_infos, write_header_csv, write_book_csv
 
-# 
-header = "product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description," \
-            "category,review_rating,image_url\n"
+# CREE LE FICHIER ET ECRIT LE HEADER
+# PARAMETRES : chaines de caracteres du header et de la categorie
+# RETOURNE : le nom du fichier créé
+def write_header_csv(string_header, category):
+    filename = category # nom par défaut
 
-
-# ECRIT LES INFOS DES LIVRES D'UNE CATEGORIE
-# PARAMETRE : l'url de la page de la categorie concernee
-def books_infos_by_category(url):
-    next = True         # booleen pour détection du bouton next
-    first = True        # booleen pour recuperation de la categorie et creation du fichier à la première boucle
-
-    category = ""
-    filename = ""
-
-    total_articles = 0
-
-    base_url = url[:-10]
+    # Creation repertoire csv si besoin
+    dir_csv = "csv"
+    isExist = os.path.exists(dir_csv)
+    if not isExist:
+        os.makedirs(dir_csv)
+        print("creation du repertoire " + dir_csv)
     
-    while next:
-        page = rq.get(url)
+    horodatage = dt.now()
+    horodatage = horodatage.strftime('-%Y-%m-%d-%H-%M-%S')
+    filename = dir_csv + "/" + filename + horodatage + ".csv"
 
-        if page.status_code == 200:   # code 200 = récup OK
-            # pour recherche dans le code html
-            soup = bs(page.content, 'html.parser')
+    # ouvre un fichier livre.csv en écriture ou le créé
+    f = open(filename, 'w', encoding='utf-8')
+    
+    # ecrit le header du fichier csv
+    f.write(string_header)
 
-            articles = soup.find_all("article")
+    # ferme le fichier
+    f.close()
 
-            # si premiere boucle on recupere la categorie et on cree le fichier avec le header
-            if first:
-                category = soup.find("li", class_="active").get_text()
-                filename = write_header_csv(header, category)
-                first = False
-                print("En cours de traitement : " + category)
-            
-            balises_a = []
-            for a in articles:
-                # recherche sur la balise <h3> car unique par article
-                tmp = a.find("h3")
-                balises_a.append(tmp.find("a"))
-
-            liste_url = []
-            for balise in balises_a:
-                # concatene l'url de base avec le lien dans la balise moins les 9 premiers caracteres            
-                liste_url.append("https://books.toscrape.com/catalogue/"+balise["href"][9:])    
-
-            # pour chaque url / article on recupere les infos et on ecrit dans le fichier
-            for url in liste_url:
-                book_infos = get_book_infos(url, category)
-                write_book_csv(book_infos, filename)
-                total_articles += 1
-
-            li_next = soup.find("li", class_="next")
-            # si bouton next on créé une nouvelle url et on boucle
-            if li_next != None:
-                next_url_end = li_next.find("a")["href"]
-                url = base_url + next_url_end
-            else:  # sinon on termine la boucle while
-                next = False
-
-        else:
-            print("Page categorie " + url + "non trouvée")
-    print(str(total_articles) + " elements")
-
->>>>>>> 7a04d610a0e7fd243978d64bcdf4938decacd3bd
-  
+    return filename
--- p2_phase3.py
+++ p2_phase3.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 # IMPORTS
 from bs4 import BeautifulSoup as bs
 import requests as rq
-from p2_phase1 import write_header_csv
 from p2_phase2 import books_infos_by_category
 
 #
@@ -39,47 +37,3 @@ def all_books_infos(url):
         
     else:
         print("page site non trouvée")
-
-=======
-# IMPORTS
-from bs4 import BeautifulSoup as bs
-import requests as rq
-from p2_phase1 import write_header_csv
-from p2_phase2 import books_infos_by_category
-
-#
-base_url = "https://books.toscrape.com/"
-
-
-# ECRIT LES FICHIERS CSV POURTOUS LES LIVRES
-# PARAMETRE : L'url du site
-# RETOURNE : aucun retour
-def all_books_infos(url):
-    first = True
-
-    page = rq.get(url)
-
-    if page.status_code == 200:   # code 200 = récup OK
-        
-        # pour recherche dans le code html
-        soup = bs(page.content, 'html.parser')
-
-        # recuperation des categories et des url des catégories par le menu gauche
-        balise_div = soup.find("div", class_="side_categories")
-        balises_a = balise_div.find_all("a")
-
-        liste_cat_url = []
-        for a in balises_a:
-            liste_cat_url.append(base_url+a["href"])
-        
-        # 1er à ignorer (non pertinent, Books / index.html)
-        for cat_url in liste_cat_url:
-            if first == True:
-                first = False
-            else:
-                books_infos_by_category(cat_url)
-        
-    else:
-        print("page site non trouvée")
-
->>>>>>> 7a04d610a0e7fd243978d64bcdf4938decacd3bd
