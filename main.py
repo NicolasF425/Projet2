a7be03ee21a2efe86540362b9b6f637f11017581
NicolasF425 <nifan42@gmail.com>
2024-11-11 12:27:01 +0100

    reupload d'un main correct

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # IMPORTS
 from p2_phase3 import all_books_infos
 
@@ -7,14 +6,4 @@ url_site = "https://books.toscrape.com/index.html"
 
 
 # EXECUTION DU PROGRAMME
-=======
-# IMPORTS
-from p2_phase3 import all_books_infos
-
-#
-url_site = "https://books.toscrape.com/index.html"
-
-
-# EXECUTION DU PROGRAMME
->>>>>>> 7a04d610a0e7fd243978d64bcdf4938decacd3bd
 all_books_infos(url_site)
